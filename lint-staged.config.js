6756305a7c8ef62f227c97269d709678ce849e5f
raychou <shun.1.zhou@kone.com>
2026-08-27 16:15:19 +0800

    chore: speed up staged linting

--- lint-staged.config.js
+++ lint-staged.config.js
@@ -1,8 +1,8 @@
 const PRETTIER_WRITE = 'prettier --write --ignore-path .prettierignore';
 
 module.exports = {
-  'src/**/*.{js,jsx,ts,tsx}': 'pnpm run lint',
-  '**/*': ['eslint --fix', 'bash -c tsc-files --noEmit', PRETTIER_WRITE],
+  'src/**/*.{js,jsx,ts,tsx}': 'eslint --cache --fix',
+  '**/*': ['bash -c tsc-files --noEmit', PRETTIER_WRITE],
   'src/**/!(*locales)/**/!(*.test|*.spec).{ts,tsx,js,go,md,mdx,yml,yaml,json,scss,css}':
     'cspell -c cspell.config.json --no-progress --no-summary',
 };
